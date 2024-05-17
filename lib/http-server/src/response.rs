e242b34be3035f3cc3b9a76496aec434a0a0d9f4
daniel <fundan.fundan@gmail.com>
2024-05-17 15:21:06 +0300

    Implement Debug and add variant checking functions for http_server::Response

--- lib/http-server/src/response.rs
+++ lib/http-server/src/response.rs
@@ -2,6 +2,7 @@ use pheidippides_utils::http::Header;
 use tokio::sync::mpsc::UnboundedReceiver;
 use crate::event_source::EventSourceEvent;
 
+#[derive(Debug)]
 pub enum Response {
     Html {
         content: String,
@@ -26,4 +27,55 @@ pub enum Response {
     BadRequest,
     InternalServerError,
     Empty,
+}
+
+impl Response {
+    pub fn is_html(self) -> bool {
+        match self {
+            Response::Html {..} => true,
+            _ => false,
+        }
+    }
+    pub fn is_text(self) -> bool {
+        match self {
+            Response::Text {..} => true,
+            _ => false,
+        }
+    }
+    pub fn is_json(self) -> bool {
+        match self {
+            Response::Json {..} => true,
+            _ => false,
+        }
+    }
+    pub fn is_redirect(self) -> bool {
+        match self {
+            Response::Redirect {..} => true,
+            _ => false,
+        }
+    }
+    pub fn is_event_source(self) -> bool {
+        match self {
+            Response::EventSource {..} => true,
+            _ => false,
+        }
+    }
+    pub fn is_bad_request(self) -> bool {
+        match self {
+            Response::BadRequest => true,
+            _ => false,
+        }
+    }
+    pub fn is_internal_server_error(self) -> bool {
+        match self {
+            Response::InternalServerError => true,
+            _ => false,
+        }
+    }
+    pub fn is_empty(self) -> bool {
+        match self {
+            Response::Empty => true,
+            _ => false,
+        }
+    }
 }
